ae5b5c9b8aab23507d254c7c8b5029341058a413
SoyJorgeDiaz5 <soyjorgediaz5@gmail.com>
2022-10-31 22:31:00 -0500

    feat(slideshow): creates Slide widget

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -1,14 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
 class SlideShowPage extends StatelessWidget {
   const SlideShowPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
-        child: SvgPicture.asset('assets/svg/mobile_feed.svg'),
+        child: _Slides(),
       ),
+    );
+  }
+}
+
+class _Slides extends StatelessWidget {
+  const _Slides({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: const [
+        _Slide(svg: 'assets/svg/mobile_feed.svg'),
+        _Slide(svg: 'assets/svg/mobile_ux.svg'),
+        _Slide(svg: 'assets/svg/online_world.svg'),
+      ],
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  const _Slide({
+    Key? key,
+    required this.svg,
+  }) : super(key: key);
+
+  final String svg;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: const EdgeInsets.all(30),
+      child: SvgPicture.asset(svg),
     );
   }
 }
