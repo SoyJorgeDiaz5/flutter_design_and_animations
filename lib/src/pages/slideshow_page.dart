29cbe9e81a97ebf6de2af4840004bd1a52214fac
SoyJorgeDiaz5 <soyjorgediaz5@gmail.com>
2022-11-11 23:07:47 -0500

    feat(slideshow): updates SlideshowPage to send slide widgets

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
 import 'package:design_and_animations/src/widgets/slideshow.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SlideshowPage extends StatelessWidget {
   const SlideshowPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Slideshow());
+    return Scaffold(
+      body: Slideshow(
+        slides: [
+          SvgPicture.asset('assets/svg/mobile_feed.svg'),
+          SvgPicture.asset('assets/svg/mobile_ux.svg'),
+          SvgPicture.asset('assets/svg/online_world.svg'),
+        ],
+      ),
+    );
   }
 }
